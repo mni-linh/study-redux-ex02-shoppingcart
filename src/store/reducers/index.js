cd602063989fd0496c71670a13f21d7a9cf0cf52
Trần Thị Tứ Linh <tranthitulinh1305@gmail.com>
2023-03-27 17:05:41 +0700

    Update index.js

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import counterReducers from "./counterReducers";
-import productsReducers from "./productsReducers";
+// import productsReducers from "./productsReducers";
 const rootReducer = combineReducers({
   counter: counterReducers,
   // products: productsReducers,
